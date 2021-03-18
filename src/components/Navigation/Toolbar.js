79969bcc6cb100d3b5c3d17cf521742d442c5cf1
Omrishoham <omri.s13@gmail.com>
2021-03-18 23:31:40 +0200

    edit readme file

--- src/components/Navigation/Toolbar.js
+++ src/components/Navigation/Toolbar.js
@@ -2,12 +2,13 @@ import React from 'react';
 import classes from './Toolbar.module.css';
 import Logo from '../Logo/Logo';
 import NavigationItems from '../Navigation/NavigationItems/NavigationItems'
+import DrawerToggle from '../../components/Navigation/SideDrawer/DrawerToggle/DrawerToggle'
 
 const toolbar = (props)=>(
     <header className={classes.Toolbar}>
-        <div>Menu</div>
-        <Logo />
-        <nav>
+        <DrawerToggle clicked={props.drawerToggleClicked} />
+        <Logo height="80%"/>
+        <nav className={classes.DesktopOnly}>
             <NavigationItems />
         </nav>
     </header>
